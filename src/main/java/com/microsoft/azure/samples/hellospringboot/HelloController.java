d2e2fd333d446155f37f1a264946dd3e9800ddcd
Subhranshu Pujari <subh.pujari@gmail.com>
2019-01-04 23:00:18 +0530

    package updated

--- src/main/java/com/microsoft/azure/samples/hellospringboot/HelloController.java
+++ src/main/java/com/microsoft/azure/samples/hellospringboot/HelloController.java
@@ -1,4 +1,4 @@
-package com.azure.test.demoazuredevops;
+package com.microsoft.azure.samples.hellospringboot;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
