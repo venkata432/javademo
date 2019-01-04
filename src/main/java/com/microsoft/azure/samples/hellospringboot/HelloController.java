c60ffe9dc77f095aec2ea1efe16e92dec502100f
Subhranshu Pujari <subh.pujari@gmail.com>
2019-01-04 22:54:53 +0530

    updated

--- src/main/java/com/microsoft/azure/samples/hellospringboot/HelloController.java
+++ src/main/java/com/microsoft/azure/samples/hellospringboot/HelloController.java
@@ -4,9 +4,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
+@RequestMapping("/test")
 public class HelloController {
 
-    @RequestMapping("/test")
+    @RequestMapping("/Greetings")
     public String index() {
         return "Greetings from Spring Boot!";
     }
