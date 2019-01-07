2d1fc53630be175e5cdc7db7ed99fd02e68a9417
subhranshu pujari <subh.pujari@gmail.com>
2019-01-07 22:58:41 +0530

    change in table name

--- src/main/java/com/microsoft/azure/samples/hellospringboot/entity/EmployeeEntity.java
+++ src/main/java/com/microsoft/azure/samples/hellospringboot/entity/EmployeeEntity.java
@@ -1,12 +1,9 @@
 package com.microsoft.azure.samples.hellospringboot.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name="employee")
 public class EmployeeEntity {
 	 
 	 @Id
