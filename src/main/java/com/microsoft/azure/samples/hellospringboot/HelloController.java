b8a2e35dec48c98933a99a5ec7c18e7ad4df1a90
Subhranshu Pujari <subh.pujari@gmail.com>
2019-01-07 12:37:51 +0530

    change in controller

--- src/main/java/com/microsoft/azure/samples/hellospringboot/HelloController.java
+++ src/main/java/com/microsoft/azure/samples/hellospringboot/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
 
     @RequestMapping("/Greetings")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Greetings from Spring Boot App!";
     }
 
 }
